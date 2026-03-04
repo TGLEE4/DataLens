e82f47de35d11af17046a09abf592473c353f698
TGLEE4 <tenglee4@gmail.com>
2026-03-04 05:37:49 -0600

    Add setup script for Python package installation
    Add installation commands for required Python packages and instructions for creating a configuration file.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip install matplotlib seaborn scipy scikit-learn textblob pandas numpy
+```
+
+Then create another new file called `.streamlit/config.toml` — click **Add file → Create new file** and type the name as:
+```
+.streamlit/config.toml
